842cdf327f62574414af5c9a29503a06f1af4458
rakibulislam8226 <rakibulislam8226@gmail.com>
2022-12-09 19:27:50 +0600

    active and total user query added

--- graphql_advance/graph_ql/events/my_new_app/schema.py
+++ graphql_advance/graph_ql/events/my_new_app/schema.py
@@ -2,10 +2,13 @@ import graphene
 from . import types
 from . import resolvers
 from . import mutations
+from django.contrib.auth.models import User
 
 
 class MyNewAppQueries(graphene.ObjectType):
     hello = graphene.String(default_value="Hi!")
+    total_user = graphene.String(default_value=len(User.objects.all()))
+    active_user = graphene.String(default_value=len(User.objects.filter(is_active=True)))
     all_category = graphene.List(types.CategoryType)
     ingredients = graphene.List(types.IngredientsType)
 
